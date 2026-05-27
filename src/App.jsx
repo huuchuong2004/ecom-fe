72e895bfc04877f78a15d85236560bae6b1d6d3c
huuchuong2004 <huuchuong398@gmail.com>
2026-05-28 01:11:52 +0700

    fix api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-const BASE_URL = 'http://110.172.28.19:8080';
+const BASE_URL = '';
 const DEFAULT_PAGE = 'landing';
 
 function App() {
